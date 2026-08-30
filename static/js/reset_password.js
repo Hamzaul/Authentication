ba2313fdda00738f678ee8df2555fdf9b9e49955
HAMZAUL RAHMAN <hamzaulrahman436@gmail.com>
2026-08-30 22:34:01 +0530

    Fix fetch call in reset password form submission

--- static/js/reset_password.js
+++ static/js/reset_password.js
@@ -2,14 +2,14 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("resetForm");
   const msg = document.getElementById("resetMsg");
 
-  form.addEventListener("submit",  (e) => {
+  form.addEventListener("submit", async (e) => {
     e.preventDefault();
 
     const password = document.getElementById("newPassword").value;
     const token = document.getElementById("resetToken").value;
 
     try {
-      const res = await ("http://127.0.0.1:5000/api/reset-password", {
+      const res = await fetch("http://127.0.0.1:5000/api/reset-password", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ token, password }),
