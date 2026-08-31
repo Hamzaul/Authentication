6d61bb46892309f38405b20ca6761ee855708630
HAMZAUL RAHMAN <hamzaulrahman436@gmail.com>
2026-08-31 22:52:20 +0530

    Make forgot password form submission asynchronous

--- static/js/forgot_password.js
+++ static/js/forgot_password.js
@@ -1,4 +1,4 @@
-document.getElementById("forgotForm").addEventListener("submit", as (e) => {
+document.getElementById("forgotForm").addEventListener("submit", async (e) => {
   e.preventDefault();
   const email = document.getElementById("forgotEmail").value;
 
